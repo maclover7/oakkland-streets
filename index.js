d1dedb58bae271b2639f6d4f7c2a99412f603f3c
Jon Moss <me@jonathanmoss.me>
2021-06-30 21:35:13 -0400

    Parse Oakland streets from ward + street data

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const { readFile } = require('fs').promises;
 const turf = require('@turf/turf');
 
-const getStreets = (wardPolygon) => {
+const getCityStreets = () => {
   return openJSONFile('./alleghenycounty_streetcenterlines202106.geojson')
   .then((features) => {
     return Promise.resolve(
       features.features.filter((f) => f.properties.RMUNI === "PITTSBURGH")
     );
   });
+};
 
-  return Promise.resolve([]);
+const getOaklandStreets = ([ward, streets]) => {
+  return Promise.resolve(
+    streets.filter((street) => turf.booleanIntersects(ward, street))
+  );
+};
+
+const getOaklandWard = () => {
+  return openJSONFile('./Pittsburgh_Wards.geojson')
+  .then((wards) => {
+    return Promise.resolve(
+      wards.features.filter((ward) => ward.properties.ward === 4)[0]
+    );
+  });
 };
 
 const listStreets = (streets) => {
-  console.log(streets);
+  console.log(streets.map((street) => street.properties.FULL_NAME));
 }
 
 const openJSONFile = (filename) => {
@@ -21,22 +34,12 @@ const openJSONFile = (filename) => {
   .then((file) => Promise.resolve(JSON.parse(file)));
 };
 
-const openWardFile = () => {
-  return openJSONFile('./Pittsburgh_Wards.geojson');
-};
-
-const selectOakland = (wards) => {
-  const ward = wards.features.filter((ward) => { return ward.properties.ward === 4; })[0]
-
-  return Promise.resolve(
-    turf.multiPolygon(ward.geometry.coordinates, { name: 'ward' })
-  );
-};
-
 const run = () => {
-  openWardFile()
-  .then(selectOakland)
-  .then(getStreets)
+  return Promise.all([
+    getOaklandWard(),
+    getCityStreets()
+  ])
+  .then(getOaklandStreets)
   .then(listStreets);
 };
 
