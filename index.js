a44eec4580a491223399243a8cc30ec5cbc5ffa9
Jon Moss <me@jonathanmoss.me>
2021-06-30 21:27:42 -0400

    Begin to parse City streets via County data

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { readFile } = require('fs').promises;
+const turf = require('@turf/turf');
 
-const getStreets = (ward) => {
+const getStreets = (wardPolygon) => {
+  return openJSONFile('./alleghenycounty_streetcenterlines202106.geojson')
+  .then((features) => {
+    return Promise.resolve(
+      features.features.filter((f) => f.properties.RMUNI === "PITTSBURGH")
+    );
+  });
+
   return Promise.resolve([]);
 };
 
@@ -8,14 +16,20 @@ const listStreets = (streets) => {
   console.log(streets);
 }
 
-const openWardFile = () => {
-  return readFile('./Pittsburgh_Wards.geojson', { encoding: 'utf8' })
+const openJSONFile = (filename) => {
+  return readFile(filename, { encoding: 'utf8' })
   .then((file) => Promise.resolve(JSON.parse(file)));
 };
 
+const openWardFile = () => {
+  return openJSONFile('./Pittsburgh_Wards.geojson');
+};
+
 const selectOakland = (wards) => {
+  const ward = wards.features.filter((ward) => { return ward.properties.ward === 4; })[0]
+
   return Promise.resolve(
-    wards.features.filter((ward) => { return ward.properties.ward === 4; })[0]
+    turf.multiPolygon(ward.geometry.coordinates, { name: 'ward' })
   );
 };
 
